d9c014ce9a63a182a8d241b831a7865351c65e14
baqnngg <il8696546@gmail.com>
2024-08-12 23:24:36 +0900

    어쩔

--- Assets/Scripts/boss_enemy/page_one/dash.cs
+++ Assets/Scripts/boss_enemy/page_one/dash.cs
@@ -3,21 +3,28 @@ using UnityEngine;
 
 public class dash : MonoBehaviour
 {
-    [SerializeField] private float dashSpeed = 20f;
-    [SerializeField] private float cooldownTime = 5f;
+    [SerializeField] private float dashSpeed = 10f;
+    [SerializeField] private float cooldownTime = 1f;
 
     public bool DS = false; // 대쉬 활성화 여부를 제어하는 변수
 
-    private bool isCooldown = false; // 대쉬 쿨타임 여부
     public bool isDashing = false; // 대쉬 중 여부
     public bool isPreparingToDash = false; // 대쉬 준비 중 여부
 
     private Vector2 dashTarget; // 대쉬 목표 위치
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TryStartDash(other.transform.position);
+        }
+    }
+
     // 대쉬 시작
     public void TryStartDash(Vector2 targetPosition)
     {
-        if (DS && !isCooldown && !isDashing) // DS가 true일 때만 대쉬를 시도
+        if (DS && !isPreparingToDash)
         {
             Debug.Log("대쉬");
             StartCoroutine(PrepareAndDash(targetPosition));
@@ -42,13 +49,7 @@ public class dash : MonoBehaviour
         }
 
         isDashing = false; // 대쉬 종료
-        isCooldown = true; // 쿨타임 시작
         yield return new WaitForSeconds(cooldownTime); // 쿨타임 대기
-        isCooldown = false; // 쿨타임 종료
     }
 
-    // 대쉬 상태를 확인하는 프로퍼티
-    public bool IsDashing { get { return isDashing; } }
-    public bool IsPreparingToDash { get { return isPreparingToDash; } }
-    public bool IsCooldown { get { return isCooldown; } }
 }
