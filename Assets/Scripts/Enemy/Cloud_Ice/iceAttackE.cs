f7a56b458b115786234f51667a0f3692cc68d901
kogsial <kogisal099@gmail.com>
2024-08-18 21:06:57 +0900

    sss

--- Assets/Scripts/Enemy/Cloud_Ice/iceAttackE.cs
+++ Assets/Scripts/Enemy/Cloud_Ice/iceAttackE.cs
@@ -15,7 +15,7 @@ public class iceAttackE : MonoBehaviour
             {
                 Quaternion currentRotation = this.transform.rotation;
                 GameObject explosion = Instantiate(iceEffect, this.transform.position, currentRotation);
-                Destroy(explosion, 0.7f);
+                Destroy(explosion, 0.4f);
             }
 
             Destroy(gameObject);
