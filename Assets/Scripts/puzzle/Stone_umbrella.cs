e42397151d9a29ae14fa276f032eb812959d5927
junhek2525 <135084879+junhek2525@users.noreply.github.com>
2024-08-23 23:09:05 +0900

    puzzle
    12

--- Assets/Scripts/puzzle/Stone_umbrella.cs
+++ Assets/Scripts/puzzle/Stone_umbrella.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stone_umbrella : MonoBehaviour
+public class stoneumbrella : MonoBehaviour
 {
+    public GameObject umbrellaObject;
+    public Itmepuzzle Itmepuzzle;
+    public happy happy;
+    /*public Transform player;*/
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +19,28 @@ public class Stone_umbrella : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("attack") || other.CompareTag("Player"))
+        {
+            Debug.Log("¸Â¾Ò´ç");
+            Itmepuzzle.stoneumbrella = true;
+            Destroy(gameObject);
+
+
+        }
+
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.T))
+        {
+
+            Itmepuzzle.stoneumbrella = false;
+            Debug.Log("dkskdskasds");
+
+
+        }
+    }
 }
+
+
+
--- Assets/Scripts/puzzle/happy_rock.cs
+++ Assets/Scripts/puzzle/happy_rock.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class happy : MonoBehaviour
 {
+    public GameObject happyrock;
+    public GameObject Stoneumbrella;
+    public stonedoor stonedoor;
+
+    public int Priority;
+    public string Stoneumbrellastate; // none close open
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,12 @@ public class happy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*GameObject attention = Instantiate(Stoneumbrella);
+        attention.transform.position = Stoneumbrella.transform.position;*/
+        if (Stoneumbrellastate == "open")
+        {
+            stonedoor.stonedooropen(Priority);
+        }
     }
+
 }
