e42b0f1ed2e267c904f1a6c3cefa2cb3e9a8ef43
Salkogi07 <145998936+Salkogi07@users.noreply.github.com>
2024-08-30 22:23:40 +0900

    ssss

--- Assets/Scripts/Player/PlayerHp.cs
+++ Assets/Scripts/Player/PlayerHp.cs
@@ -7,6 +7,8 @@ public class PlayerHp : MonoBehaviour
     public int player_HP = 0;
     private int player_maxHP = 100;
     private bool isDead;
+    public float invincibilityDuration = 0.5f;
+    public GameObject effect;
     public ShakeCam Shaking;
 
     [SerializeField] Image player_HpBar;
@@ -14,10 +16,16 @@ public class PlayerHp : MonoBehaviour
     public GameObject menu;
 
     PlayerMove playerMove;
+    SpriteRenderer sprite;
+    Rigidbody2D rb;
+
+    public bool isInvincible  = false;
 
     private void Awake()
     {
         playerMove = GetComponent<PlayerMove>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Start()
@@ -34,15 +42,34 @@ public class PlayerHp : MonoBehaviour
 
     public void Damage_HP(int _value)
     {
+        if (isInvincible)
+            return;
+
         if (!playerMove.isDashing)
         {
             Shaking.Shake();
+            GameObject effectObj = Instantiate(effect, transform.position, Quaternion.identity);
+            Destroy(effectObj, 0.5f);
             player_HP -= _value;
             Set_HP(player_HP);
+
+            // 무적 상태 시작
+            StartCoroutine(InvincibilityCoroutine());
+
         }
     }
 
-    
+    private IEnumerator InvincibilityCoroutine()
+    {
+        isInvincible = true;
+        sprite.color = new Color(1, 1, 1, 0.5f);
+
+        yield return new WaitForSeconds(invincibilityDuration);
+
+        isInvincible = false;
+        sprite.color = new Color(1, 1, 1, 1f);
+    }
+
 
     private void Set_HP(int _value)
     {
